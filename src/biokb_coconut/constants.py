abe8e66ee379d9b9c021831a069ed92e67b5c468
Christian Ebeling <Christian.Ebeling@scai.fraunhofer.de>
2025-12-05 14:03:32 +0100

    several new links

--- src/biokb_coconut/constants.py
+++ src/biokb_coconut/constants.py
@@ -3,7 +3,6 @@
 import os
 from pathlib import Path
 
-
 HOME = str(Path.home())
 BIOKB_FOLDER = os.path.join(HOME, ".biokb")
 DB_DEFAULT_CONNECTION_STR = "sqlite:///" + os.path.join(BIOKB_FOLDER, "biokb.db")
@@ -11,9 +10,31 @@ PROJECT_NAME = "coconut"
 TABLE_PREFIX = f"{PROJECT_NAME}_"
 PROJECT_FOLDER = os.path.join(BIOKB_FOLDER, PROJECT_NAME)
 DATA_FOLDER = os.path.join(PROJECT_FOLDER, "data")
-
 os.makedirs(DATA_FOLDER, exist_ok=True)
+WCVP_NAMES_FILE = os.path.join(BIOKB_FOLDER, "wcvp", "data")
+WCVP_ZIP_FILE = "wcvp.zip"
+WCVP_DATA_FOLDER = os.path.join(BIOKB_FOLDER, "wcvp", "data")
+os.makedirs(WCVP_DATA_FOLDER, exist_ok=True)
+WCVP_ZIP_FILE_PATH = os.path.join(WCVP_DATA_FOLDER, WCVP_ZIP_FILE)
+WCVP_NAMES_FILE = "wcvp_names.csv"
 
-DOWNLOAD_LINK = (
-    "https://coconut.s3.uni-jena.de/prod/downloads/2025-07/coconut_csv-07-2025.zip"
-)
+
+# get current year and month
+from datetime import datetime
+
+month = datetime.now().month
+year = datetime.now().year
+
+DOWNLOAD_LINK = f"https://coconut.s3.uni-jena.de/prod/downloads/{year}-{month:02d}/coconut_csv-{month:02d}-{year}.zip"
+FILE_IN_ZIP = f"coconut_csv-{month:02d}-{year}.csv"
+WCVP_DOWNLOAD_URL = "https://sftp.kew.org/pub/data-repositories/WCVP/wcvp.zip"
+
+TAXONOMY_URL = "https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdmp.zip"
+TAXONOMY_DATA_FOLDER = os.path.join(BIOKB_FOLDER, "taxtree", "data")
+os.makedirs(TAXONOMY_DATA_FOLDER, exist_ok=True)
+
+BASIC_NODE_LABEL = "DbCoconut"
+
+EXPORT_FOLDER = os.path.join(DATA_FOLDER, "ttls")
+os.makedirs(EXPORT_FOLDER, exist_ok=True)
+TAXONOMY_DATA_FOLDER = os.path.join(BIOKB_FOLDER, "taxtree", "data")
