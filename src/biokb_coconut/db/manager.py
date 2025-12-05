afa469dd65230836ee0b45f5a6b1c5a00f675943
Christian Ebeling <Christian.Ebeling@scai.fraunhofer.de>
2025-12-05 14:02:50 +0100

    normalization of data

--- src/biokb_coconut/db/manager.py
+++ src/biokb_coconut/db/manager.py
@@ -1,15 +1,16 @@
+import io
 import logging
 import os
-import re
 import urllib.request
 import zipfile
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Type
 from urllib.parse import urlparse
 
 import pandas as pd
+import requests
 from pandas import DataFrame, Series
-from sqlalchemy import Engine, create_engine, text
+from sqlalchemy import Engine, create_engine, text, update
 from sqlalchemy.orm import sessionmaker
 
 from biokb_coconut import constants
@@ -80,82 +81,126 @@ class DbManager:
             urllib.request.urlretrieve(constants.DOWNLOAD_LINK, path_to_file)
         return path_to_file
 
+    def insert_one_to_n_table(
+        self, df: DataFrame, column_name: str, model: Type[models.Base]
+    ) -> DataFrame:
+        df_cc = (
+            df[[column_name]]
+            .drop_duplicates()
+            .dropna()
+            .rename(columns={column_name: "name"})
+            .reset_index(drop=True)
+            .rename_axis("id")
+        )
+        df_cc.index += 1  # Start index from 1
+        df_cc.to_sql(model.__tablename__, self.engine, if_exists="append")
+        df_cc[f"{column_name}_id"] = df_cc.index
+        return df.merge(
+            df_cc,
+            how="left",
+            left_on=column_name,
+            right_on="name",
+            suffixes=("", f"_{column_name}"),
+        ).drop(columns=[f"name_{column_name}"])
+
     def import_data(self):
         """Import data from SQL file and use mappings to import data."""
         self.recreate_db()
         path_to_file = self.path_to_file or self.download_data()
 
         logger.info("Importing data from %s", path_to_file)
-        # Unzip the test.zip file in the data folder
-        with zipfile.ZipFile(path_to_file, "r") as zip_ref:
-            # Get the names of extracted files (in this case only a single file) to use as reference below
-            extracted = zip_ref.namelist()
-            zip_ref.extractall(constants.DATA_FOLDER)
+        df = pd.read_csv(
+            path_to_file,
+            compression="zip",
+            low_memory=False,
+        )
 
-        csv_path = os.path.join(constants.DATA_FOLDER, extracted[0])
+        df.rename(
+            columns={"cas": "cas_numbers"}, inplace=True
+        )  # create a plural to allow for easier handling
+
+        df = self.insert_one_to_n_table(
+            df, "chemical_super_class", models.ChemicalSuperClass
+        )
+        df = self.insert_one_to_n_table(df, "chemical_class", models.ChemicalClass)
+        df = self.insert_one_to_n_table(
+            df, "chemical_sub_class", models.ChemicalSubClass
+        )
+        df = self.insert_one_to_n_table(
+            df, "direct_parent_classification", models.DirectParentClassification
+        )
+        df = self.insert_one_to_n_table(
+            df, "np_classifier_pathway", models.NpClassifierPathway
+        )
+        df = self.insert_one_to_n_table(
+            df, "np_classifier_superclass", models.NpClassifierSuperclass
+        )
+        df = self.insert_one_to_n_table(
+            df, "np_classifier_class", models.NpClassifierClass
+        )
+
         column_names = [
             column.name
             for column in models.Compound.__table__.columns
             if column.name != "id"
         ]
-
-        df = pd.read_csv(csv_path, low_memory=False)
-        df.rename(
-            columns={"cas": "cas_numbers"}, inplace=True
-        )  # create a plural to allow for easier handling
-        df.index = df.index + 1  # Adjust index to start from 1
+        df.reset_index(inplace=True)
+        df.index += 1
         df.index.name = "id"
         inserted_rows = df[column_names].to_sql(
             models.Compound.__tablename__,
             self.engine,
             if_exists="append",
             chunksize=100000,
-        )  # Select only the relevant columns
+        )
 
         logger.info("Data imported into %s", models.Compound.__tablename__)
 
         self.import_n2m_column(
             column_series=df.collections,
             column_name=models.Collection.name.name,
-            joining_model=models.compound_collection,
+            joining_model=models.CompoundCollection,
             model=models.Collection,
         )
 
         self.import_n2m_column(
             column_series=df.organisms,
             column_name=models.Organism.name.name,
-            joining_model=models.compound_organism,
+            joining_model=models.CompoundOrganism,
             model=models.Organism,
         )
 
         self.import_n2m_column(
             column_series=df.dois,
             column_name=models.DOI.identifier.name,
-            joining_model=models.compound_doi,
+            joining_model=models.CompoundDOI,
             model=models.DOI,
         )
 
         self.import_n2m_column(
             column_series=df.synonyms,
             column_name=models.Synonym.name.name,
-            joining_model=models.compound_synonym,
+            joining_model=models.CompoundSynonym,
             model=models.Synonym,
         )
 
         self.import_n2m_column(
             column_series=df.cas_numbers,
             column_name=models.CAS.number.name,
-            joining_model=models.compound_cas,
+            joining_model=models.CompoundCAS,
             model=models.CAS,
         )
-        return {"inserted_rows": inserted_rows}
+
+        self.update_organism_tax_ids()
+        self.update_other_organism_ids_by_wcvp()
+        return inserted_rows
 
     def import_n2m_column(
         self,
         column_series: Series,
         column_name: str,
-        joining_model,
-        model,
+        joining_model: Type[models.Base],
+        model: Type[models.Base],
     ):
         index_on: str = str(column_series.name)
 
@@ -179,12 +224,160 @@ class DbManager:
         df_1.join(df_2, how="inner").drop_duplicates(
             subset=["compound_id", index_on[:-1] + "_id"]
         ).to_sql(
-            joining_model.name,
+            joining_model.__tablename__,
             self.engine,
             if_exists="append",
             index=False,
         )
         logger.info(
             "Imported %s",
-            joining_model.name,
+            joining_model.__tablename__,
         )
+
+    def __download_taxdmp(self, path_to_file: str):
+        """Download the NCBI taxdump file."""
+        if not os.path.exists(path_to_file):
+            logger.info("Download taxonomy data")
+            r = requests.get(
+                constants.TAXONOMY_URL,
+                allow_redirects=True,
+            )
+            open(path_to_file, "wb").write(r.content)
+
+    def _import_tax_names(self):
+        """Import the taxonomy names.
+
+        Returns:
+            Dict[str, int]: table name, number of entries
+        """
+        logger.info("import taxonomy names (up to 5min)")
+        models.TaxonomyName.__table__.drop(self.engine, checkfirst=True)  # type: ignore
+        models.TaxonomyName.__table__.create(self.engine, checkfirst=True)  # type: ignore
+        taxtree_path_to_file = os.path.join(
+            constants.TAXONOMY_DATA_FOLDER, "taxdmp.zip"
+        )
+        self.__download_taxdmp(taxtree_path_to_file)
+        archive = zipfile.ZipFile(taxtree_path_to_file, "r")
+        names = archive.read("names.dmp")
+        df = pd.read_csv(
+            io.StringIO(names.decode("utf-8")),
+            sep=r"\t\|\t",
+            engine="python",
+            usecols=[0, 1, 3],
+            names=["tax_id", "name", "name_type"],
+        )
+        df.name_type = df.name_type.str[:-2]
+        df.index += 1
+        df.index.rename("id", inplace=True)
+        df.to_sql(
+            models.TaxonomyName.__tablename__,
+            self.engine,
+            if_exists="append",
+            chunksize=10000,
+        )
+
+    def update_organism_tax_ids(self, import_taxonomy_names: bool = True):
+        """Update the tax_ids in the organism table.
+
+        Uses NCBI Taxonomy names to find tax_ids for plant names in the organism table.
+
+        First tries to match scientific names, then any name.
+        """
+        logger.info("Update tax_ids in organism table (up to 5min)")
+        if import_taxonomy_names:
+            self._import_tax_names()
+
+        with self.Session() as session:
+            # Scientific name
+            logger.info("Update tax_ids by scientific names")
+            stmt = (
+                update(models.Organism)
+                .where(
+                    models.Organism.name == models.TaxonomyName.name,
+                    models.TaxonomyName.name_type == "scientific name",
+                )
+                .values(tax_id=models.TaxonomyName.tax_id)
+            )
+            session.execute(stmt)
+            session.commit()
+
+            # Try any name
+            stmt = (
+                update(models.Organism)
+                .where(
+                    models.Organism.name == models.TaxonomyName.name,
+                    models.Organism.tax_id.is_(None),
+                )
+                .values(tax_id=models.TaxonomyName.tax_id)
+            )
+            session.execute(stmt)
+            session.commit()
+        models.TaxonomyName.__table__.drop(self.engine, checkfirst=True)  # type: ignore
+
+    def update_other_organism_ids_by_wcvp(self):
+        # download WCVP data if not already exists
+        logger.info("Update other organism ids by WCVP")
+
+        models.WCVPPlant.__table__.drop(self.engine, checkfirst=True)  # type: ignore
+        models.WCVPPlant.__table__.create(self.engine, checkfirst=True)  # type: ignore
+        if not os.path.exists(constants.WCVP_ZIP_FILE_PATH):
+            logger.info("Downloading WCVP data...")
+            urllib.request.urlretrieve(
+                constants.WCVP_DOWNLOAD_URL, constants.WCVP_ZIP_FILE_PATH
+            )
+
+        with zipfile.ZipFile(constants.WCVP_ZIP_FILE_PATH, "r") as zf:
+            use_cols = [
+                "plant_name_id",
+                "taxon_name",
+                "taxon_rank",
+                "accepted_plant_name_id",
+                "powo_id",
+                "ipni_id",
+            ]
+            df = pd.read_csv(
+                zf.open(constants.WCVP_NAMES_FILE), usecols=use_cols, sep="|"
+            )
+            df.set_index("plant_name_id", inplace=True)
+            df[df["taxon_rank"] == "Species"].drop(columns=["taxon_rank"]).to_sql(
+                models.WCVPPlant.__tablename__,
+                self.engine,
+                if_exists="append",
+                chunksize=10000,
+            )
+        with self.Session() as session:
+            stmt = (
+                update(models.Organism)
+                .where(
+                    models.Organism.name == models.WCVPPlant.taxon_name,
+                    models.WCVPPlant.plant_name_id
+                    == models.WCVPPlant.accepted_plant_name_id,
+                )
+                .values(
+                    ipni_id=models.WCVPPlant.ipni_id,
+                    powo_id=models.WCVPPlant.powo_id,
+                    wcvp_id=models.WCVPPlant.plant_name_id,
+                )
+            )
+            session.execute(stmt)
+            session.commit()
+
+            stmt = (
+                update(models.Organism)
+                .where(
+                    models.Organism.name == models.WCVPPlant.taxon_name,
+                    models.Organism.wcvp_id.is_(None),
+                    models.WCVPPlant.plant_name_id
+                    != models.WCVPPlant.accepted_plant_name_id,
+                    models.WCVPPlant.accepted_plant_name_id.is_not(None),
+                )
+                .values(
+                    ipni_id=models.WCVPPlant.ipni_id,
+                    powo_id=models.WCVPPlant.powo_id,
+                    wcvp_id=models.WCVPPlant.accepted_plant_name_id,
+                )
+            )
+            session.execute(stmt)
+            session.commit()
+
+        models.WCVPPlant.__table__.drop(self.engine, checkfirst=True)  # type: ignore
--- src/biokb_coconut/db/models.py
+++ src/biokb_coconut/db/models.py
@@ -1,6 +1,14 @@
 from typing import Optional
 
-from sqlalchemy import Column, ForeignKey, Index, Integer, String, Table, Text
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Index,
+    Integer,
+    String,
+    Text,
+    UniqueConstraint,
+)
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 from biokb_coconut import constants
@@ -10,67 +18,73 @@ class Base(DeclarativeBase):
     table_prefix = constants.PROJECT_NAME + "_"
 
 
-# Association tables for many-to-many relationships
-compound_organism = Table(
-    Base.table_prefix + "compound_organism",
-    Base.metadata,
-    Column(
-        "compound_id", ForeignKey(Base.table_prefix + "compound.id"), primary_key=True
-    ),
-    Column(
-        "organism_id", ForeignKey(Base.table_prefix + "organism.id"), primary_key=True
-    ),
-)
-
-compound_collection = Table(
-    Base.table_prefix + "compound_collection",
-    Base.metadata,
-    Column(
-        "compound_id", ForeignKey(Base.table_prefix + "compound.id"), primary_key=True
-    ),
-    Column(
-        "collection_id",
-        ForeignKey(Base.table_prefix + "collection.id"),
-        primary_key=True,
-    ),
-)
-
-compound_synonym = Table(
-    Base.table_prefix + "compound_synonym",
-    Base.metadata,
-    Column(
-        "compound_id", ForeignKey(Base.table_prefix + "compound.id"), primary_key=True
-    ),
-    Column(
-        "synonym_id", ForeignKey(Base.table_prefix + "synonym.id"), primary_key=True
-    ),
-)
-
-compound_cas = Table(
-    Base.table_prefix + "compound_cas",
-    Base.metadata,
-    Column(
-        "compound_id", ForeignKey(Base.table_prefix + "compound.id"), primary_key=True
-    ),
-    Column("cas_number_id", ForeignKey(Base.table_prefix + "cas.id"), primary_key=True),
-)
-
-compound_doi = Table(
-    Base.table_prefix + "compound_doi",
-    Base.metadata,
-    Column(
-        "compound_id", ForeignKey(Base.table_prefix + "compound.id"), primary_key=True
-    ),
-    Column("doi_id", ForeignKey(Base.table_prefix + "doi.id"), primary_key=True),
-)
+# abstract base class for tables with only unique name field
+class OnlyName:
+    __tablename__: str
+    id: Mapped[int]
+    name: Mapped[str]
+    compounds: Mapped[list["Compound"]]
 
 
+# many-to-many association tables
+class CompoundOrganism(Base):
+    __tablename__ = Base.table_prefix + "compound_organism"
+
+    compound_id: Mapped[int] = mapped_column(
+        ForeignKey(Base.table_prefix + "compound.id"), primary_key=True
+    )
+    organism_id: Mapped[int] = mapped_column(
+        ForeignKey(Base.table_prefix + "organism.id"), primary_key=True
+    )
+
+
+class CompoundCollection(Base):
+    __tablename__ = Base.table_prefix + "compound_collection"
+
+    compound_id: Mapped[int] = mapped_column(
+        ForeignKey(Base.table_prefix + "compound.id"), primary_key=True
+    )
+    collection_id: Mapped[int] = mapped_column(
+        ForeignKey(Base.table_prefix + "collection.id"), primary_key=True
+    )
+
+
+class CompoundSynonym(Base):
+    __tablename__ = Base.table_prefix + "compound_synonym"
+
+    compound_id: Mapped[int] = mapped_column(
+        ForeignKey(Base.table_prefix + "compound.id"), primary_key=True
+    )
+    synonym_id: Mapped[int] = mapped_column(
+        ForeignKey(Base.table_prefix + "synonym.id"), primary_key=True
+    )
+
+
+class CompoundCAS(Base):
+    __tablename__ = Base.table_prefix + "compound_cas"
+
+    compound_id: Mapped[int] = mapped_column(
+        ForeignKey(Base.table_prefix + "compound.id"), primary_key=True
+    )
+    cas_number_id: Mapped[int] = mapped_column(
+        ForeignKey(Base.table_prefix + "cas.id"), primary_key=True
+    )
+
+
+class CompoundDOI(Base):
+    __tablename__ = Base.table_prefix + "compound_doi"
+
+    compound_id: Mapped[int] = mapped_column(
+        ForeignKey(Base.table_prefix + "compound.id"), primary_key=True
+    )
+    doi_id: Mapped[int] = mapped_column(
+        ForeignKey(Base.table_prefix + "doi.id"), primary_key=True
+    )
+
+
+# other tables
 class Compound(Base):
     __tablename__ = Base.table_prefix + "compound"
-    __table_args__ = {
-        "mysql_charset": "utf8",
-        "mysql_collate": "utf8_unicode_ci",
-    }
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     identifier: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
@@ -99,43 +113,152 @@ class Compound(Base):
     fractioncsp3: Mapped[float]
     number_of_minimal_rings: Mapped[int]
     van_der_walls_volume: Mapped[Optional[float]]
-    contains_sugar: Mapped[bool]
+    contains_sugar: Mapped[Optional[bool]]
     contains_ring_sugars: Mapped[bool]
     contains_linear_sugars: Mapped[bool]
     murcko_framework: Mapped[Optional[str]] = mapped_column(Text)
     np_likeness: Mapped[float]
-    chemical_class: Mapped[Optional[str]] = mapped_column(String(255))
-    chemical_sub_class: Mapped[Optional[str]] = mapped_column(String(255))
-    chemical_super_class: Mapped[Optional[str]] = mapped_column(String(255))
-    direct_parent_classification: Mapped[Optional[str]] = mapped_column(String(255))
-    np_classifier_pathway: Mapped[Optional[str]] = mapped_column(String(255))
-    np_classifier_superclass: Mapped[Optional[str]] = mapped_column(String(255))
-    np_classifier_class: Mapped[Optional[str]] = mapped_column(String(255))
+    np_classifier_pathway_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey(Base.table_prefix + "np_classifier_pathway.id")
+    )
+    np_classifier_superclass_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey(Base.table_prefix + "np_classifier_superclass.id")
+    )
+    np_classifier_class_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey(Base.table_prefix + "np_classifier_class.id")
+    )
     np_classifier_is_glycoside: Mapped[Optional[bool]]
 
+    # foreign keys to classification tables
+    chemical_class_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey(Base.table_prefix + "chemical_class.id")
+    )
+    chemical_sub_class_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey(Base.table_prefix + "chemical_sub_class.id")
+    )
+    direct_parent_classification_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey(Base.table_prefix + "direct_parent_classification.id")
+    )
+    chemical_super_class_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey(Base.table_prefix + "chemical_super_class.id")
+    )
+    # relationships
+    chemical_class: Mapped[Optional["ChemicalClass"]] = relationship(
+        back_populates="compounds"
+    )
+    chemical_sub_class: Mapped[Optional["ChemicalSubClass"]] = relationship(
+        back_populates="compounds"
+    )
+    direct_parent_classification: Mapped[Optional["DirectParentClassification"]] = (
+        relationship(back_populates="compounds")
+    )
+    chemical_super_class: Mapped[Optional["ChemicalSuperClass"]] = relationship(
+        back_populates="compounds"
+    )
+    np_classifier_pathway: Mapped[Optional["NpClassifierPathway"]] = relationship(
+        back_populates="compounds"
+    )
+    np_classifier_superclass: Mapped[Optional["NpClassifierSuperclass"]] = relationship(
+        back_populates="compounds"
+    )
+    np_classifier_class: Mapped[Optional["NpClassifierClass"]] = relationship(
+        back_populates="compounds"
+    )
+
     organisms: Mapped[list["Organism"]] = relationship(
-        secondary=compound_organism, back_populates="compounds"
+        secondary=CompoundOrganism.__table__, back_populates="compounds"
     )
     collections: Mapped[list["Collection"]] = relationship(
-        secondary=compound_collection, back_populates="compounds"
+        secondary=CompoundCollection.__table__, back_populates="compounds"
     )
     dois: Mapped[list["DOI"]] = relationship(
-        secondary=compound_doi, back_populates="compounds"
+        secondary=CompoundDOI.__table__, back_populates="compounds"
     )
     synonyms: Mapped[list["Synonym"]] = relationship(
-        secondary=compound_synonym, back_populates="compounds"
+        secondary=CompoundSynonym.__table__, back_populates="compounds"
     )
     cas_numbers: Mapped[list["CAS"]] = relationship(
-        secondary=compound_cas, back_populates="compounds"
+        secondary=CompoundCAS.__table__, back_populates="compounds"
+    )
+
+
+class NpClassifierPathway(Base, OnlyName):
+    __tablename__ = Base.table_prefix + "np_classifier_pathway"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True)
+
+    compounds: Mapped[list["Compound"]] = relationship(
+        back_populates="np_classifier_pathway"
+    )
+
+
+class NpClassifierSuperclass(Base, OnlyName):
+    __tablename__ = Base.table_prefix + "np_classifier_superclass"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True)
+
+    compounds: Mapped[list["Compound"]] = relationship(
+        back_populates="np_classifier_superclass"
+    )
+
+
+class NpClassifierClass(Base, OnlyName):
+    __tablename__ = Base.table_prefix + "np_classifier_class"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True)
+
+    compounds: Mapped[list["Compound"]] = relationship(
+        back_populates="np_classifier_class"
+    )
+
+
+class ChemicalClass(Base, OnlyName):
+    __tablename__ = Base.table_prefix + "chemical_class"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True)
+
+    compounds: Mapped[list["Compound"]] = relationship(back_populates="chemical_class")
+
+
+class ChemicalSubClass(Base, OnlyName):
+    __tablename__ = Base.table_prefix + "chemical_sub_class"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True)
+
+    compounds: Mapped[list["Compound"]] = relationship(
+        back_populates="chemical_sub_class"
+    )
+
+
+class DirectParentClassification(Base, OnlyName):
+    __tablename__ = Base.table_prefix + "direct_parent_classification"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True)
+
+    compounds: Mapped[list["Compound"]] = relationship(
+        back_populates="direct_parent_classification"
+    )
+
+
+class ChemicalSuperClass(Base, OnlyName):
+    __tablename__ = Base.table_prefix + "chemical_super_class"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True)
+
+    compounds: Mapped[list["Compound"]] = relationship(
+        back_populates="chemical_super_class"
     )
 
 
 class DOI(Base):
     __tablename__ = Base.table_prefix + "doi"
-    __table_args__ = {
-        "mysql_charset": "utf8",
-        "mysql_collate": "utf8_unicode_ci",
-    }
 
     id: Mapped[int] = mapped_column(primary_key=True)
     identifier: Mapped[str] = mapped_column(
@@ -143,50 +266,51 @@ class DOI(Base):
         unique=True,
     )
     compounds: Mapped[list["Compound"]] = relationship(
-        secondary=compound_doi, back_populates="dois"
+        secondary=CompoundDOI.__table__, back_populates="dois"
     )
 
 
 class Synonym(Base):
     __tablename__ = Base.table_prefix + "synonym"
-    __table_args__ = {
-        "mysql_charset": "utf8",
-        "mysql_collate": "utf8_unicode_ci",
-    }
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(
         String(1000).with_variant(String(1000, collation="utf8mb4_bin"), "mysql")
     )
     compounds: Mapped[list["Compound"]] = relationship(
-        secondary=compound_synonym, back_populates="synonyms"
+        secondary=CompoundSynonym.__table__, back_populates="synonyms"
     )
     __table_args__ = (Index("uq_my_model_name", "name", unique=True, mysql_length=768),)
 
 
 class Organism(Base):
     __tablename__ = Base.table_prefix + "organism"
-    __table_args__ = {
-        "mysql_charset": "utf8",
-        "mysql_collate": "utf8_unicode_ci",
-    }
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(
-        String(255).with_variant(String(255, collation="utf8mb4_bin"), "mysql"),
-        unique=True,
+        String(1000).with_variant(
+            String(length=1000, collation="utf8mb4_bin"), "mysql"
+        ),
     )
+    tax_id: Mapped[Optional[int]] = mapped_column(index=True)
+    ipni_id: Mapped[Optional[str]] = mapped_column(String(255))
+    wcvp_id: Mapped[Optional[int]]
+    powo_id: Mapped[Optional[str]] = mapped_column(String(255))
     compounds: Mapped[list["Compound"]] = relationship(
-        secondary=compound_organism, back_populates="organisms"
+        secondary=CompoundOrganism.__table__, back_populates="organisms"
+    )
+
+    __table_args__ = (
+        Index(
+            f"ux_{__tablename__}__name",
+            name,
+            mysql_length=255,
+        ),
     )
 
 
 class Collection(Base):
     __tablename__ = Base.table_prefix + "collection"
-    __table_args__ = {
-        "mysql_charset": "utf8",
-        "mysql_collate": "utf8_unicode_ci",
-    }
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(
@@ -194,7 +318,7 @@ class Collection(Base):
         unique=True,
     )
     compounds: Mapped[list["Compound"]] = relationship(
-        secondary=compound_collection, back_populates="collections"
+        secondary=CompoundCollection.__table__, back_populates="collections"
     )
 
     @property
@@ -204,15 +328,49 @@ class Collection(Base):
 
 class CAS(Base):
     __tablename__ = Base.table_prefix + "cas"
-    __table_args__ = {
-        "mysql_charset": "utf8",
-        "mysql_collate": "utf8_unicode_ci",
-    }
+
     id = Column(Integer, primary_key=True)
     number = Column(
         String(255).with_variant(String(255, collation="utf8mb4_bin"), "mysql"),
         unique=True,
     )
     compounds: Mapped[list["Compound"]] = relationship(
-        secondary=compound_cas, back_populates="cas_numbers"
+        secondary=CompoundCAS.__table__, back_populates="cas_numbers"
+    )
+
+
+class TaxonomyName(Base):
+    """Class definition for table taxonomy_name. Name from
+    NCBI taxonomy https://www.ncbi.nlm.nih.gov/taxonomys."""
+
+    __tablename__ = Base.table_prefix + "taxonomy_name"
+    __table_args__ = {"comment": "Taxonomy names by NCBI"}
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tax_id: Mapped[int] = mapped_column(index=True, comment="NCBI taxonomy Identifier")
+    name: Mapped[str] = mapped_column(Text)
+    name_type: Mapped[str] = mapped_column(String(255), index=True)
+
+    __table_args__ = (
+        Index(
+            f"ix_{__tablename__}__name",
+            name,
+            mysql_length=255,
+        ),
+    )
+
+
+class WCVPPlant(Base):
+    __tablename__ = Base.table_prefix + "wcvp_plant"
+    plant_name_id: Mapped[int] = mapped_column(primary_key=True)
+    taxon_name: Mapped[Optional[str]] = mapped_column(Text)
+    accepted_plant_name_id: Mapped[Optional[int]] = mapped_column(index=True)
+    powo_id: Mapped[Optional[str]] = mapped_column(String(255))
+    ipni_id: Mapped[Optional[str]] = mapped_column(String(255))
+
+    __table_args__ = (
+        Index(
+            f"ix_{__tablename__}__taxon_name",
+            taxon_name,
+            mysql_length=255,
+        ),
     )
